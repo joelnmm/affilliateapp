d1bc909fd47c9400a97513b40485fc6c9b44e656
Joel Nestor Males Morales <jmales@libelulasoft.com>
2022-11-04 12:26:32 -0500

    products page updated
    products page updated

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -14,6 +14,7 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/site.css',
         'css/style_products.css',
+        'css/mediumish.css',
     ];
     public $js = [
     ];
