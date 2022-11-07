a32fe1c346e87024c41a71ce0532d913d27b0303
Joel Nestor Males Morales <jmales@libelulasoft.com>
2022-11-06 23:50:47 -0500

    default url updated
    default url updated

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -76,8 +76,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
-        // return Yii::$app->response->redirect(['site/productos']); 
+        // return $this->render('index');
+        return Yii::$app->response->redirect(['site/productos']); 
     }
 
     /**
